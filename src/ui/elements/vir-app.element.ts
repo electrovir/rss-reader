ab6ebb76bfd897ab8a8729ea3421db913192b9f8
electrovir <electrovir@users.noreply.github.com>
2026-01-13 16:48:19 +0000

    init

---
+++ src/ui/elements/vir-app.element.ts
@@ -0,0 +1,10 @@
+import {defineElement, html} from 'element-vir';
+
+export const VirApp = defineElement()({
+    tagName: 'vir-app',
+    render() {
+        return html`
+            Vir App goes here!
+        `;
+    },
+});
